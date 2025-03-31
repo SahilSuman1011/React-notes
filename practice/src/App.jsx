ea1b7272d0c44ef88618515f4f155a285ffc1616
Sahil Suman <sahilsuman1202@gmail.com>
2025-03-31 12:00:06 +0530

    usestate

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -6,14 +6,17 @@ function App() {
   function increaseCount() {
     setCount(currentValue => currentValue + 1);
   }
+
   useEffect(function() {
     console.log("above setInterval")
     setInterval(increaseCount, 1000);
   }, [])
 
-  // [] this is called ddependency array which is
+  // [] this is called dependency array which is
   // used to control the execution of useEffect hook.
-  
+
+  // This effect will run on mount, because array is empty.
+
   return (
     <div>
        {count}
