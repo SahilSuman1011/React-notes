979a35c2bb39d0303c6d1037074e03b15e8e633a
Sahil Suman <sahilsuman1202@gmail.com>
2025-03-31 09:43:50 +0530

    useEffect code

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,39 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 function App() {
 
   const[count, setCount] = useState(1);
   
   function increaseCount() {
-    setCount(count + 1);
+    setCount(currentValue => currentValue + 1);
   }
-  setInterval(increaseCount, 3000);
+  useEffect(function() {
+    console.log("above setInterval")
+    setInterval(increaseCount, 1000);
+  }, [])
 
+  // [] this is called ddependency array which is
+  // used to control the execution of useEffect hook.
+  
   return (
-    <div style={{ display: "flex"}}>
-      <div
-        style={{
-          background: "aqua",
-          borderRadius: 20,
-          width: 25,
-          height: 30,
-          paddingLeft: 13,
-          paddingTop: 5,
-        }}
-      >
-        {count}
+    <div>
+       {count}
       </div>
-      <img
-        style={{ cursor: "pointer" }}
-        src="https://imgs.search.brave.com/HkSkuh3l5WMMAS2QGB4PPujXteptm_xiTFH_N8xVHX0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA1LzI4LzAyLzI3/LzM2MF9GXzUyODAy/Mjc5MF9lU0VvdHZ2/eXpabHV1R2cxWHFs/c2F0UUszU1VyQ3Zy/Ny5qcGc"
-        width={40}
-        
-      />
-     
-    </div>
   );
 }
 
 export default App;
 
 //useEffect Hook : helps to perform side effects in function components,
-// code runs on mount
+// code runs only on mount
